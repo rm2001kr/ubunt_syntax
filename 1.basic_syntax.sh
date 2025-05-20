77a171a60da1e943647e926a329edaa36f0d1c11
rm2001kr <icebag0721@naver.com>
2025-05-20 12:49:57 +0900

    commit

--- 1.basic_syntax.sh
+++ 1.basic_syntax.sh
@@ -1,3 +1,4 @@
+# 2025/5/20 Linux 요약
 # 현재 위치 경로를 출력
 pwd
 
@@ -34,3 +35,68 @@ cd
 #상대경로와 절대경로
 #상대경로 : cd ../../
 #절대경로 : cd /home
+
+
+# 2025년 5월 20일 Linux 요약 2
+#비어있는 파일 만들기
+touch first_file.txt
+
+#파일내용 터미널창에 출력하기
+cat first_file.txt
+
+#파일내용 상위10줄 출력하기
+head first_file.txt
+
+#파일내용 하위 10줄 출력하기
+tail first_file.txt
+
+#파일내용 상/하위 n개 조회
+tail -n 파일명
+
+#파일내용 실시간 조회
+tail -f 파읾여
+
+#파일내용 편집기 : nano, vi
+#도스키 위/아래를 통해 이전명령어 조회
+#이전에 실행했던 명령어 조회
+history
+
+#현재 내 터미널창에 보이는 명령어 내역 정리
+clear
+
+# ^c 는 입력명령어 삭제
+
+#파일삭제는 rm 파일명, -f 옵션을 통해 묻지않고 삭제
+rm 파일명 
+
+#폴더째 삭제
+rm -r 폴더명
+
+# 터미널창에 문자열 출력
+echo "hello world"
+
+# echo를 통해 파일에 내용 입력 
+# > : 덮어쓰기, >> : 추가모드
+echo "hello world" > first_file.txt
+
+# cp는 복사명령어 
+# cp 복사대상 복사될파일명
+# 폴더 복사시에는 -r 옵션 
+cp 1_file.txt 2_file.txt
+cp 1_file.txt ../2_file.txt
+
+# grep : 파일 내에서 문자열 찾기
+# r:모든 디렉토리 내, n : 라인수 출력, i : 대, 소문자 구분 없이
+grep -rni "hello" .
+
+# find : 이름으로 파일 또는 디렉토리 찾기
+find . -name "*.txt"
+find . -type f 
+
+# find와 grap의 혼용
+find -name "*.txt" | xargs grep -rni "hello"
+find -name "*.txt" -exec grep -rni "hello" {} \;
+
+# 타입은 file인 파일 중에 (-type f) first로 시작하고 
+# 그 파일들 안에 hello 라는 키워드가 있는 문장 찾기
+find . -name "first*" -type f | xargs grep -rni "hello"
---
+++ user_manaegent.sh
@@ -0,0 +1,13 @@
+# 사용자 추가 및 비밀번호 지정
+SUDO useradd newuser1
+SUDO passwd newuser1
+
+# 사용자 변경 : 변경하고자 하는 계정의 비밀번호
+# sudo의 경우 현재 사용자의 비밀번호
+su - newuser1
+
+# chmod는 권한부여 : r은 4, w는 2, x는 1, u(소유자), g(그룹), o(그외)로 구성
+chmod 644 (644 해석하면 r+w,r,r) 파일명
+chmod u+x 파일명
+chmod u-x 파일명
+chmod u=r 파일명
