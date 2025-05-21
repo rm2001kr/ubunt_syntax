2c52a628fe5ce3f2d25c365a8e7aa752c494e854
rm2001kr <icebag0721@naver.com>
2025-05-21 14:06:01 +0900

    commit

--- 3. shell programming.sh
+++ 3. shell programming.sh
@@ -5,9 +5,9 @@ nano myscript.sh
 echo "hello world" 
 chmod u+x myscript.sh
 # 스크립트 실행
-./myscript.sh
+./myscript.sh (현재 폴더에 이 파일을 실행하겠다 라는 의미)
 
-##################실습################# [1]
+##################실습################# [tester1]
 # 터미널창에 "script practice start" 문구 출력
 # 홈 디렉토리에서 mydir이라는 폴더 생성
 # mydir로 이동해서 file1.txt, file2.txt 파일 생성 (touch 사용 )
@@ -17,7 +17,7 @@ chmod u+x myscript.sh
 # file2.txt파일은 이름은 file_rename.txt로 변경
 # 터미널창에 "script practice end" 문구 입력
 
-#############################################
+######################################
 
 # myscript2 생성 후 아래 코드 작성 및 실행
 # [2] if문 
@@ -55,4 +55,4 @@ do
 done
 echo "count value is $count"
 
-# for문 및 변수값 세팅 활용
+# for문 및 변수값 세팅 활용 [tester2]
---
+++ 4. Netwrok_syntax.sh
@@ -0,0 +1,43 @@
+# 프로세스 목록조회 
+ps -ef 
+ps -ef | grep "nginx"
+
+# 패키지 관련
+# yum은 redhat
+# apt 또는 apt-get은 데비안
+
+# 패키지 목록 최신화
+sudo apt update
+
+# #nginx 설치
+sudo apt install nginx
+
+# 프로그램 실행 관리 도구 : systemctl
+sudo systemctl start nginx
+sudo sywtemctl stop nginx
+
+# 프로세스 강제 종료
+kill -9 프로세스ID
+
+# 네트워크 관련한 명령어
+# 로컬 ip 정보 조회 : ipconfig
+
+# 특정 도메인의 ip 주소 정보 조회 : DNS 서버의 문의
+nslookup abc.com
+
+# 네트워크 연결 상태 조회
+ping IP주소
+telnet
+nc -zvㅁ
+# ip와 포트를 이용해, 특정 프로그램의 이용가능 상태까지 확인
+# 일반적으로 telnet 막혀있거나 사용장 임의 설정으로 못하는 경우 발생
+tlenet   ip주소 포트번호
+nc -zv ip주소 (또는 포트번호)
+
+# 원녁접속
+
+# 원격파일 전송
+
+# 원격 접속 : 22port
+ssh username@도메이누소 (또는 ip 주소)
+scp 파일전송
